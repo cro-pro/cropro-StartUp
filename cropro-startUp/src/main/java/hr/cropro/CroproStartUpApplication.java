1e8c6e420f6ab4fdd6f70c02e742cbf33907b775
Mario Majdandzic <mmajdandzic@rao.hr>
2016-10-19 11:49:01 +0200

    initial commit

---
+++ cropro-startUp/src/main/java/hr/cropro/CroproStartUpApplication.java
@@ -0,0 +1,12 @@
+package hr.cropro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroproStartUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CroproStartUpApplication.class, args);
+	}
+}
