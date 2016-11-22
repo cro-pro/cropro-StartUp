a7dde7dedeb628543645e6465eb46819e17f5374
Mario Majdandzic <mmajdandzic@rao.hr>
2016-11-22 09:05:15 +0100

    test

--- cropro-startUp/src/main/java/hr/cropro/controllers/GreetingController.java
+++ cropro-startUp/src/main/java/hr/cropro/controllers/GreetingController.java
@@ -3,18 +3,15 @@ package hr.cropro.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import hr.cropro.services.TestService;
 
 @Controller
 public class GreetingController {
-	
+
 	@Autowired
 	TestService testService;
 
@@ -22,33 +19,34 @@ public class GreetingController {
 	public String echo() {
 		return "Halo";
 	}
-	
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-    
-    	String msg = testService.isJaro(name);
-    	if(msg != null) {
-    		model.addAttribute("name", msg);
-    	} else {    	
-    		model.addAttribute("name", name);
-    	}
-    	
-        return "greeting";	
-    }
-    
-    @RequestMapping("/login")
-    public String greeting(Model model) {
 
-        return "login";	
-    }
-    
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String signIn(@RequestParam String username, @RequestParam String password, Model model) {
-    	
-    	model.addAttribute("name", username);
-    	model.addAttribute("password", password);
-    	
-        return "greeting";	
-    }
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+
+		String msg = testService.isJaro(name);
+		if (msg != null) {
+			model.addAttribute("name", msg);
+		} else {
+			model.addAttribute("name", name);
+		}
+
+		return "greeting";
+	}
+
+	@RequestMapping("/login")
+	public String greeting(Model model) {
+
+		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String signIn(@RequestParam String username, @RequestParam String password, Model model) {
+
+		model.addAttribute("name", username);
+		model.addAttribute("password", password);
+
+		return "greeting";
+	}
 
 }
